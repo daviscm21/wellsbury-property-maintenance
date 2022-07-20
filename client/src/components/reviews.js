7ac3182ee220ec997ade9abcc21774a8f424d232
daviscm21 <davisc1@mail.com>
2022-07-20 11:05:38 +0100

    Tenth commit

--- client/src/components/reviews.js
+++ client/src/components/reviews.js
@@ -27,13 +27,7 @@ const Reviews = () => {
             <h2 className='reviews-header'>Reviews:</h2>  
             <div className='reviews'>
                
-               {reviews && reviews.map((review, index) => {return <div key={index} className='review'>
-                   <h3>{review.title}</h3>
-                   <p>{review.review}</p>
-                   <h4>{review.firstname} {review.surname}</h4>
-                   {starsLoop(review.stars)}                  
-               </div>})
-               }
+
               </div>            
                <ReviewForm setReviews={setReviews}/>
             </div>
